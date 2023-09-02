6e66f62572927648ee25564fd9b9a64e19e29c5f
narozeol <mashironaro@gmail.com>
2023-09-02 21:38:22 +0800

    添加对教务网，扬华素质网数量的支持

--- 计院/run.sh
+++ 计院/run.sh
@@ -10,23 +10,12 @@ if ((num <= 0)); then
 fi
 
 
-if (curl -s https://scai.swjtu.edu.cn/web/page-module.html?mid=B730BEB095B31840 > __notice__.html); then
-    echo "已获取网页信息"
-else
-    echo "获取网页信息失败"
-    return 1
-fi
+curl -s https://scai.swjtu.edu.cn/web/page-module.html?mid=B730BEB095B31840 > __notice__.html
+
 
 #第一个通知    
 grep -m 1 --after-context=2 "list-top-item fl"  __notice__.html | ./markrm.o | grep '\S' | sed 's/^[ \t]*//' > msg.txt
 grep -m 1 --after-context=2 "list-top-item fl"  __notice__.html | ./webfix.o >> msg.txt
-if (($? != 0)); then
-    echo "获取第1个通知失败"
-    rm __notice__.html
-    return 1
-else
-    echo "已获取第1个通知"
-fi
 
 ((num -= 1))
 if ((num <= 0)); then
@@ -36,21 +25,7 @@ fi
 #第二个通知
 
 grep -m 1 --after-context=2 "list-top-item fr" __notice__.html | ./markrm.o | grep '\S' | sed 's/^[ \t]*//' >> msg.txt
-if (($? != 0)); then
-    echo "获取第2个通知失败"
-    rm __notice__.html
-    return 1
-else
-    echo "已获取第2个通知"
-fi
 grep -m 1 --after-context=2 "list-top-item fr" __notice__.html | ./webfix.o >> msg.txt
-if (($? != 0)); then
-    echo "获取第2个通知失败"
-    rm __notice__.html
-    return 1
-else
-    echo "已获取第2个通知"
-fi
 
 ((num -= 1))
 if ((num <= 0)); then
@@ -60,18 +35,10 @@ fi
 
 for ((i=1; i<= num; i++))
 do  
-    grep  --after-context=2 "info-wrapper fl" __notice__.html | sed -n "$[i*4-3],$[i*4-1]p" | ./markrm.o | grep '\S' | sed 's/^[ \t]*//' >> msg.txt
-    grep  --after-context=2 "info-wrapper fl" __notice__.html | sed -n "$[i*4-3],$[i*4-1]p" | ./webfix.o >> msg.txt
-    if (($? != 0)); then
-        echo "获取第$((i + 2))个通知失败"
-        rm __notice__.html
-        return 1
-    else
-        echo "已获取第$((i + 2))个通知"
-    fi
+    grep  --after-context=2 "info-wrapper fl" __notice__.html | sed -n "$((i*4-3)),$((i*4-1))p" | ./markrm.o | grep '\S' | sed 's/^[ \t]*//' >> msg.txt
+    grep  --after-context=2 "info-wrapper fl" __notice__.html | sed -n "$((i*4-3)),$((i*4-1))p" | ./webfix.o >> msg.txt
 done
 
-echo ' '
 cat msg.txt 
 
 rm __notice__.html
