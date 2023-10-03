85782875d53d532d1d3bab2f1c089bd71f823dd8
narozeol <mashironaro@gmail.com>
2023-10-03 23:54:09 +0800

    Rebulid Project, add UI

---
+++ Program/src/UI/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Microsoft YaHei UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(461, 539);
+            button1.Name = "button1";
+            button1.Size = new Size(117, 68);
+            button1.TabIndex = 0;
+            button1.Text = "教务处";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += Button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.AcceptsReturn = true;
+            textBox1.Font = new Font("Microsoft YaHei UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(12, 12);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            textBox1.Size = new Size(1264, 521);
+            textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Microsoft YaHei UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(657, 539);
+            button2.Name = "button2";
+            button2.Size = new Size(117, 68);
+            button2.TabIndex = 2;
+            button2.Text = "计院";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += Button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1288, 619);
+            Controls.Add(button2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Button button2;
+    }
+}
---
+++ Program/src/UI/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System.Xml;
+using MainFunction;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button1_Click(object sender, EventArgs e)
+        {
+            string LoadingSymbol = "⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏";
+            int i = 0;
+            Task<string> task = Notice.GetNoticeFromJWCAsync();
+
+            while (!task.IsCompleted)
+            {
+                button1.Text = LoadingSymbol[i++ % LoadingSymbol.Length].ToString();
+                await Task.Delay(100);
+            }
+
+            button1.Text = "教务处";
+            textBox1.Text = task.Result;
+        }
+
+        private async void Button2_Click(object sender, EventArgs e)
+        {
+            string LoadingSymbol = "⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏";
+            int i = 0;
+            Task<string> task = Notice.GetNoticeFromSCAIAsync();
+
+            while (!task.IsCompleted)
+            {
+                button2.Text = LoadingSymbol[i++ % LoadingSymbol.Length].ToString();
+                await Task.Delay(100);
+            }
+
+            button2.Text = "计院";
+            textBox1.Text = task.Result;
+        }
+    }
+}
