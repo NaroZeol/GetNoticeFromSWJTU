25c0e9c43616acb9638b021b6e0c0a430c9cc02f
narozeol <mashironaro@gmail.com>
2023-10-09 22:56:34 +0800

    添加了自动运行，以及通过闪烁图标来告知有新通知

---
+++ Program/Utilities/Net.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program.Utilities
+{
+    public class CheckNetwork
+    {
+        [DllImport("wininet.dll", EntryPoint = "InternetGetConnectedState")]
+        public extern static bool InternetGetConnectedState(out int conState, int reader);
+
+        public static bool IsNetworkConnected
+        {
+            get
+            {
+                return InternetGetConnectedState(out int n, 0);
+            }
+        }
+    }
+}
