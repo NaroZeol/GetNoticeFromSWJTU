3051d7283404c357ffaf6442a1eb13264811a999
narozeol <mashironaro@gmail.com>
2023-10-11 20:58:49 +0800

    修复了断网时会报错的问题（采用了相当粗暴的方式掩盖异常。。。）

--- Program/src/GetNotice/GetNotice.cs
+++ Program/src/GetNotice/GetNotice.cs
@@ -6,7 +6,18 @@ public static class GetNotice
     public async static Task<string> GetNoticeFromJWCAsync()
     {
         HtmlWeb web = new();
-        HtmlAgilityPack.HtmlDocument doc = await web.LoadFromWebAsync("http://jwc.swjtu.edu.cn/vatuu/WebAction?setAction=newsList");
+        HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+        try
+        {
+            doc = await web.LoadFromWebAsync("http://jwc.swjtu.edu.cn/vatuu/WebAction?setAction=newsList");
+        }
+        catch (Exception _)
+        {
+            FileStream fileStream = new FileStream("JWC.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            string ret = FileData.ReadFromFile(fileStream);
+            fileStream.Close();
+            return ret;
+        }
         HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes("//div[@class='littleResultDiv']");
 
         string baseUrl = "http://jwc.swjtu.edu.cn/";
@@ -26,8 +37,18 @@ public static class GetNotice
     public async static Task<string> GetNoticeFromSCAIAsync()
     {
         HtmlWeb web = new();
-        HtmlAgilityPack.HtmlDocument doc = await web.LoadFromWebAsync("https://scai.swjtu.edu.cn/web/page-module.html?mid=B730BEB095B31840");
-
+        HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+        try
+        {
+            doc = await web.LoadFromWebAsync("https://scai.swjtu.edu.cn/web/page-module.html?mid=B730BEB095B31840");
+        }
+        catch (Exception _)
+        {
+            FileStream fileStream = new FileStream("SCAI.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            string ret = FileData.ReadFromFile(fileStream);
+            fileStream.Close();
+            return ret;
+        }
         HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes("//div[@class='list-top-item fl']");
         HtmlNodeCollection nodes1 = doc.DocumentNode.SelectNodes("//div[@class='list-top-item fr']");
         nodes.Append(nodes1[0]);
@@ -58,7 +79,19 @@ public static class GetNotice
     public async static Task<string> GetNoticeFromXGBAsync()
     {
         HtmlWeb web = new();
-        HtmlAgilityPack.HtmlDocument doc = await web.LoadFromWebAsync("http://xg.swjtu.edu.cn/web/Home/PushNewsList?Lmk7LJw34Jmu=010j.shtml");
+        HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+        try
+        {
+            doc = await web.LoadFromWebAsync("http://xg.swjtu.edu.cn/web/Home/PushNewsList?Lmk7LJw34Jmu=010j.shtml");
+        }
+        catch (Exception _)
+        {
+            FileStream fileStream = new FileStream("XGB.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            string ret = FileData.ReadFromFile(fileStream);
+            fileStream.Close();
+            return ret;
+        }
+
         HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes("//div[@class='right-side']//ul[@class='block-ctxlist']//li");
 
         string baseUrl = "http://xg.swjtu.edu.cn/";
