733163b555732aae9bb9be59d23c346354414559
NaroZeol <mashironaro@gmail.com>
2024-02-22 04:55:37 +0800

    为重复的运行的实例添加了弹窗提示

--- dotnet8.0/Program/Program.cs
+++ dotnet8.0/Program/Program.cs
@@ -13,12 +13,11 @@ namespace WinFormsApp1
         {
             //用于确保只有一个实例在运行
             string mutexName = "MutexForGetNoticeFromSWJTU";
-            using (Mutex mutex = new Mutex(false, mutexName, out bool isCreateNew))
-            { 
-                if (!isCreateNew)
-                {
-                    Environment.Exit(1);
-                }
+            Mutex mutex = new Mutex(false, mutexName, out bool isCreateNew);
+            if (!isCreateNew)
+            {
+                MessageBox.Show(null, "已存在一个运行实例", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(1);
             }
 
             // To customize application configuration such as set high DPI settings or default font,
